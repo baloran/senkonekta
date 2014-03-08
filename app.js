b305834e038feeb4970ee5c1110d83deee95d5ba
Baloran <contact@baloran.fr>
2014-03-08 16:02:20 +0100

    supression de express.js + modif index.html
    Modif du index.html pour integrer le fichier app.js afin de lancer un
    serveur node. Utilisation de Http pour creer le serveur module node de
    base

--- app.js
+++ app.js
@@ -1,40 +1,9 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
-var path = require('path');
-var os = require('os');
-var gui = require('nw.gui'),
-win = gui.Window.get();
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
 
-console.log(gui);
-
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'html'});
+res.end('hello');
+console.log(index);
+}).listen(9615);
