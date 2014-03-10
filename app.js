6266e04e1899701a8addb044a4993625af061ce1
Baloran <contact@baloran.fr>
2014-03-10 01:30:09 +0100

    création du serveur nodeJS
    création d'un server nodejs basique sans ExpressJS

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
 var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
 
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'html'});
-res.end('hello');
-console.log(index);
-}).listen(9615);
+http.createServer().listen(9615);
